658c801d54635e740272a9dd448c990e6427cda8
Atilas Rodrigues Lamberti <lambertiatilas@gmail.com>
2021-11-28 22:30:35 -0300

    Adicionando senha a Cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -25,6 +25,8 @@ public class ClienteNewDTO implements Serializable {
 	private String cpfOuCnpj;
 	
 	private Integer tipo;
+	
+	private String senha;
 
 	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
@@ -81,6 +83,15 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getLogradouro() {
